404e2786630d431e7001a2a3c880691ce0a58b24
houcineaADL <eladdali.lahoucine@ine.inpt.ma>
2023-05-19 00:55:41 +0100

    screens UI

---
+++ app/src/main/java/com/example/spendwise/activities/GetStartedActivity.java
@@ -0,0 +1,7 @@
+package com.example.spendwise.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GetStartedActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/example/spendwise/activities/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.spendwise.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
